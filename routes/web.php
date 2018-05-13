dc8d0eff8c281886271bcaf1a825dd258cb6b99a
Levente Korponai <levente.korponai@stylersonline.com>
2018-05-13 17:32:10 +0200

    added closure routes

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/hello-world', function(){
+    return 'Hello world!';
+});
+
+Route::get('/hello/{name?}', function($name = 'Guest'){
+    return "Hello {$name}!";
+})->where('name', '[A-Z][a-z]+');
+
+Route::post('/post', function(){
+    return 'Hello world!';
+});
